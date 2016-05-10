304bbf10c68f9701c4791fbac637014d5a1b3f53
Richard Gowers <richardjgowers@gmail.com>
2016-05-10 23:18:16 +0100

    more tests for geom

--- tests/test_geom.cpp
+++ tests/test_geom.cpp
@@ -3,7 +3,9 @@
 
 using namespace geom;
 
-BOOST_AUTO_TEST_CASE(rad_to_def)
+BOOST_AUTO_TEST_CASE(rad_to_deg)
 {
-  BOOST_CHECK(RadToDeg(0.0) == 0.0);
+  BOOST_CHECK_CLOSE(RadToDeg(0.0), 0.0, 0.001);
+  BOOST_CHECK_CLOSE(RadToDeg(3.14159265359), 180.0, 0.001);
+  BOOST_CHECK_CLOSE(RadToDeg(- 3.14159265359), - 180.0, 0.001);
 }
